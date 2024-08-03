01a1ed0efc3dc4afb91071963de7dbe75e8c4538
Ross Morsali <ross@codeamp.io>
2024-08-03 18:34:26 +0200

    Tidy up background.js and add comments

--- background.js
+++ background.js
@@ -1,49 +1,53 @@
 
-let domainsToIgnore = '';
-
-
+// Add context menu items.
 chrome.runtime.onInstalled.addListener(() => {
+	// Add block domain context menu item.
 	chrome.contextMenus.create({
 		id: '1password-block-domain',
 		title: 'Block this domain',
 		contexts: ['page', 'action']
 	});
 
+	// Add unblock domain context menu item.
 	chrome.contextMenus.create({
 		id: '1password-unblock-domain',
 		title: 'Unblock this domain',
 		contexts: ['page', 'action']
 	})
 
-  });
+});
 
-
+// Handle context menu clicks.
 function contextClick(info, tab) {
 	const { menuItemId } = info
-	const domainName = new URL(tab.url).hostname;
-	if ( menuItemId === '1password-block-domain' ) {
 
-		// 
-		chrome.storage.sync.get(["domains"]).then((result) => {
-			domainsToIgnore = result.domains ? result.domains : '';
-			if ( ! domainsToIgnore.includes(domainName) ) {
+	// Get the domain name from the tab URL.
+	const domainName = new URL( tab.url ).hostname;
+
+	// Block the domain.
+	if ( menuItemId === '1password-block-domain' ) {
+		chrome.storage.sync.get( ["domains"] ).then( ( result ) => {
+			// Init setting.
+			let domainsToIgnore = result.domains ? result.domains : '';
+			// Add the domain to the list if it's not already there.
+			if ( ! domainsToIgnore.includes( domainName ) ) {
 				domainsToIgnore = domainsToIgnore.concat( domainName, ',' );
 			}
-			chrome.storage.sync.set({ 'domains': domainsToIgnore }).then(() => {
-				console.log("Value is set", domainsToIgnore);
-			});
+			chrome.storage.sync.set( { 'domains': domainsToIgnore } );
 		});
 	}
+
+	// Unblock the domain.
 	if ( menuItemId === '1password-unblock-domain' ) {
-		const domains = domainsToIgnore.split(',');
-		chrome.storage.sync.get(["domains"]).then((result) => {
-			domainsToIgnore = result.domains ? result.domains : '';
-			domainsToIgnore = domainsToIgnore.replaceAll(domainName + ',', '');
-			chrome.storage.sync.set({ 'domains': domainsToIgnore }).then(() => {
-				console.log("Value is set", domainsToIgnore);
-			});
+		chrome.storage.sync.get( ["domains"] ).then( ( result ) => {
+			// Init setting.
+			let domainsToIgnore = result.domains ? result.domains : '';
+			// Remove the domain from the list.
+			domainsToIgnore = domainsToIgnore.replaceAll( domainName + ',', '' );
+			chrome.storage.sync.set( { 'domains': domainsToIgnore } );
 		});
 	}
 }
 
+// Add context menu click listener.
 chrome.contextMenus.onClicked.addListener( contextClick );
