63c27822e4e6d978f8b0b83296b525fe08026049
Ross Morsali <ross@codeamp.io>
2024-08-06 15:17:53 +0200

    Fix issue when node doesn't have a tagName (ie a textnode)

--- content.js
+++ content.js
@@ -2,54 +2,73 @@
 function run() {
 
 	chrome.storage.sync.get( ["domains"] ).then( ( result ) => {
-		if ( result && result.domains ) {
-			const domainsToIgnore = result.domains.split(',');
 
-			if ( ! domainsToIgnore.includes(window.location.hostname) ) {
-				return;
-			}
+		if ( ! result ) {
+			return;
+		}
 
-			console.log("Hiding 1Password UI for domain: " + window.location.hostname);
-			
-			// The known 1Password UI tags (web components).
-			const blockTagNames = ['com-1password-notification', 'com-1password-button', 'com-1password-menu'];
+		if ( ! result.domains ) {
+			return;
+		}
 
-			// Create a hidden wrapper to move all the components into.
-			const hiddenWrapper = document.createElement('div');
-			hiddenWrapper.className = 'onepassword-block-hide-element';
-			document.body.append(hiddenWrapper);
-			
-			// Callback for the MutationObserver - move the added nodes to the hidden wrapper.
-			function callback( mutationsList, observer ) {
-				mutationsList.forEach( ( mutation ) => {
-					if ( mutation.type === 'childList' ) {
-						for ( let node of mutation.addedNodes ) {
-							if ( blockTagNames.includes( node.tagName.toLowerCase() ) ) {
-								// Don't move the node if it's already in the hidden wrapper.
-								if ( node.parentNode && ! node.parentNode.classList.contains( 'onepassword-block-hide-element' )) {
-									moveToContainer( node, hiddenWrapper );
-								}
-							}
-						}
+		const domainsToIgnore = result.domains.split(',');
+
+		if ( ! domainsToIgnore.includes( window.location.hostname ) ) {
+			return;
+		}
+
+		console.log( "Hiding 1Password UI for domain: " + window.location.hostname );
+		
+		// The known 1Password UI tags (web components).
+		const blockTagNames = ['com-1password-notification', 'com-1password-button', 'com-1password-menu'];
+
+		// Create a hidden wrapper to move all the components into.
+		const hiddenWrapper = document.createElement( 'div' );
+		hiddenWrapper.className = 'onepassword-block-hide-element';
+		document.body.append( hiddenWrapper );
+		
+		// Callback for the MutationObserver - move the added nodes to the hidden wrapper.
+		function callback( mutationsList ) {
+
+			mutationsList.forEach( ( mutation ) => {
+
+				if ( mutation.type !== 'childList' ) {
+					return;
+				}
+
+				for ( let node of mutation.addedNodes ) {
+					if ( typeof node.tagName === "undefined" ) {
+						continue;
 					}
-				} );
-			}
+					if ( ! blockTagNames.includes( node.tagName.toLowerCase() ) ) {
+						continue;
+					}
+					if ( ! node.parentNode ) {
+						continue;
+					}
+					if ( node.parentNode.classList.contains( 'onepassword-block-hide-element' ) ) {
+						continue;
+					}
 
-			// Initialize the MutationObserver.
-			const observer = new MutationObserver( callback );
-			const config = { childList: true, subtree: true };
-			observer.observe( document.body, config );
-			
-			// In case the 1Password UI is already on the page before we added the mutation observer,
-			// move those elements to the hidden wrapper.
-			const onepassElements = document.querySelectorAll( blockTagNames.join(', ') );
-			onepassElements.forEach( ( element, index ) => {
-				if ( element.parentNode && ! element.parentNode.classList.contains( 'onepassword-block-hide-element' )) {
-					moveToContainer( element, hiddenWrapper );
+					moveToContainer( node, hiddenWrapper );
 				}
 			} );
 		}
-	});
+
+		// Initialize the MutationObserver.
+		const observer = new MutationObserver( callback );
+		const config = { childList: true, subtree: true };
+		observer.observe( document.body, config );
+		
+		// In case the 1Password UI is already on the page before we added the mutation observer,
+		// move those elements to the hidden wrapper.
+		const onepassElements = document.querySelectorAll( blockTagNames.join(', ') );
+		onepassElements.forEach( ( element, index ) => {
+			if ( element.parentNode && ! element.parentNode.classList.contains( 'onepassword-block-hide-element' )) {
+				moveToContainer( element, hiddenWrapper );
+			}
+		} );
+	} );
 }
 
 function moveToContainer( element, container ) {
@@ -58,8 +77,8 @@ function moveToContainer( element, container ) {
 }
 
 // Init.
-if (document.readyState === 'complete') {
-  run();
+if ( document.readyState === 'complete' ) {
+	un();
 } else {
-  window.addEventListener('load', run);
+	window.addEventListener( 'load', run );
 }
